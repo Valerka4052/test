c1ed27dfd29b7652f0273fb17c0ae62ba05524a2
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-05-08 21:35:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <BrowserRouter basename="/test">
+                <HashRouter basename="/test">
                     <App />
-                </BrowserRouter>
+                </HashRouter>
             </PersistGate>
         </Provider>
     </React.StrictMode>
